7a0008392ba6a20f237f6ecfd85e438985526ebf
sebastian <sebastian@e4dd7656-0c73-4daa-9ac4-3c17ed980afe>
2009-08-25 20:31:41 +0000

    Some games with the headers...

--- src/ANIMATION.c
+++ src/ANIMATION.c
@@ -1,56 +1,50 @@
-
 #ifdef __dingens
 #include <p18f4520.h>
 #endif /* __dingens */
 
-#include <charlibrary.h>
-#include <incl/vardef.h>
+#include "charlibrary.h"
+#include "vardef.h"
 //#include <kanallibrary.h>
-#include <incl/CHARSELECT.h>
+#include "CHARSELECT.h"
 //#include <kanalselect.h>
-#include <incl/CHARSCALE.h>
-#include <incl/CHAROFFSET.h>
-#include <incl/CHARAPPEND.h>
-#include <incl/LDAC.h>
-#include <incl/DISPLAYCHAR.h>
+#include "CHARSCALE.h"
+#include "CHAROFFSET.h"
+#include "CHARAPPEND.h"
+#include "LDAC.h"
+#include "DISPLAYCHAR.h"
 //#include <CHARINIT.h>
-#include <incl/picturelibrary.h>
+#include "picturelibrary.h"
 //#include <math.h>
 
 
-void ANIMATION (void)
-{
+void ANIMATION(void) {
 
-anitime=9;
+	anitime = 9;
 
-  while (1)
-  {
+	while (1) {
 
-  xpicpointer=&xtheriddle_frame_000[0];
-  ypicpointer=&ytheriddle_frame_000[0];
-  ANIBILD();
-  xpicpointer=&xtheriddle_frame_001[0];
-  ypicpointer=&ytheriddle_frame_001[0];
-  ANIBILD();
-  xpicpointer=&xtheriddle_frame_002[0];
-  ypicpointer=&ytheriddle_frame_002[0];
-  ANIBILD();
-  xpicpointer=&xtheriddle_frame_003[0];
-  ypicpointer=&ytheriddle_frame_003[0];
-  ANIBILD();
-  xpicpointer=&xtheriddle_frame_004[0];
-  ypicpointer=&ytheriddle_frame_004[0];
-  ANIBILD();
-  xpicpointer=&xtheriddle_frame_005[0];
-  ypicpointer=&ytheriddle_frame_005[0];
-  ANIBILD();
-  xpicpointer=&xtheriddle_frame_006[0];
-  ypicpointer=&ytheriddle_frame_006[0];
-  ANIBILD();
+		xpicpointer = &xtheriddle_frame_000[0];
+		ypicpointer = &ytheriddle_frame_000[0];
+		ANIBILD();
+		xpicpointer = &xtheriddle_frame_001[0];
+		ypicpointer = &ytheriddle_frame_001[0];
+		ANIBILD();
+		xpicpointer = &xtheriddle_frame_002[0];
+		ypicpointer = &ytheriddle_frame_002[0];
+		ANIBILD();
+		xpicpointer = &xtheriddle_frame_003[0];
+		ypicpointer = &ytheriddle_frame_003[0];
+		ANIBILD();
+		xpicpointer = &xtheriddle_frame_004[0];
+		ypicpointer = &ytheriddle_frame_004[0];
+		ANIBILD();
+		xpicpointer = &xtheriddle_frame_005[0];
+		ypicpointer = &ytheriddle_frame_005[0];
+		ANIBILD();
+		xpicpointer = &xtheriddle_frame_006[0];
+		ypicpointer = &ytheriddle_frame_006[0];
+		ANIBILD();
 
-  
-  }
-  
-
+	}
 
 }
